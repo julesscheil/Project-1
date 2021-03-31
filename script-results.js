4de960f6682ec82d435b2655508dd1649dcabae0
Matt Hale <mhale@factharbor.com>
2021-03-31 19:43:15 -0400

    formatted results.js

--- script-results.js
+++ script-results.js
@@ -1,18 +1,14 @@
-function getApi() {
+function edamamQuery() {
   // fetch request gets a list of all the repos for the node.js organization
-  var requestUrl =
-    "https://api.edamam.com/search?q=christmas&app_id=6896e3c1&app_key=810173b6ecf9f3abd5c456c48ec0a9cc";
+  var edamamUrl = "https://api.edamam.com/search?q=christmas&app_id=6896e3c1&app_key=810173b6ecf9f3abd5c456c48ec0a9cc";
 
-  fetch(
-    requestUrl
-   
-    
-  )
+  fetch(edamamUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data.hits);
+      console.log(data.hits[0]);
     });
 }
-getApi();
+
+edamamQuery();
