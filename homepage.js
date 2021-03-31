0ef792b89d7fb82b9fe8cc32a3e43b93e79e6ca4
Ian F <ian.ferg36@gmail.com>
2021-03-31 11:35:05 -0600

    added current year with moment to parameters

--- homepage.js
+++ homepage.js
@@ -1,13 +1,16 @@
 // Psuedocode
 // 1. Set up moment
 var todaysDate = moment().format("MMM Do YY");
-console.log(todaysDate);
+var currentYear = moment().format("YYYY");
+
 // 2. fetch calendarific data (console log to test)
 var apiKey = "4e543c39030cf814f23d6b0384c5df95bb916d89";
 var apiURL =
   "https://calendarific.com/api/v2/holidays?&api_key=" +
   apiKey +
-  "&country=US&year=2019";
+  "&country=US&year=" +
+  currentYear +
+  "&type=national,religious";
 function getCalendarAPI() {
   fetch(apiURL)
     .then(function (response) {
