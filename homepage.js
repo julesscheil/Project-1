715e9753628203f380060b0bcbdc6040e39570ac
Ian F <ian.ferg36@gmail.com>
2021-03-31 11:23:05 -0600

    set up variables and a succesful fetch to calendarific

---
+++ homepage.js
@@ -0,0 +1,23 @@
+// Psuedocode
+// 1. Set up moment
+var todaysDate = moment().format("MMM Do YY");
+console.log(todaysDate);
+// 2. fetch calendarific data (console log to test)
+var apiKey = "4e543c39030cf814f23d6b0384c5df95bb916d89";
+var apiURL =
+  "https://calendarific.com/api/v2/holidays?&api_key=" +
+  apiKey +
+  "&country=US&year=2019";
+function getCalendarAPI() {
+  fetch(apiURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+getCalendarAPI();
+// 3. click event for submit button
+// 4. access calendar data
+// 5. Append upcoming holidays to front page list
