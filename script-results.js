11d72e8e330c8ef0292320c87951485de51ff841
Jackson Peak <jacksonpeak1@icloud.com>
2021-03-31 14:49:18 -0400

    Updated api key to console log in new file

--- script-results.js
+++ script-results.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+function getApi() {
+  // fetch request gets a list of all the repos for the node.js organization
+  var requestUrl =
+    "https://api.edamam.com/search?q=christmas&app_id=6896e3c1&app_key=810173b6ecf9f3abd5c456c48ec0a9cc";
+
+  fetch(
+    requestUrl
+   
+    
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.hits);
+    });
+}
+getApi();
